68d1ad0816d0bd59a93055d0cebabb8bdd61bfae
Usama Naeem <45305195+Usama137@users.noreply.github.com>
2021-11-25 13:46:41 +0500

    Update globalVariables.dart

--- lib/globalVariables.dart
+++ lib/globalVariables.dart
@@ -3,7 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:riderapp/dataModels/user.dart';
 
-String mapKey='AIzaSyBBdMTXGcPdWKJbiQBCe5-SKIOKGeDlWdo';
+String mapKey='';
 
  final CameraPosition googlePlex = CameraPosition(
   target: LatLng(37.42796133580664, -122.085749655962),
